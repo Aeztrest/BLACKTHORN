97166ad5c6c896f6b29ab4f9b306df930404b9ba
Ahmet Buğra Kurnaz <ahmetbugrakurnaz0@gmail.com>
2026-05-04 15:53:16 +0300

    str

--- blackthron_docs/src/components/Logo.tsx
+++ blackthron_docs/src/components/Logo.tsx
@@ -1,15 +1,7 @@
 export function Logo(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
     <svg viewBox="0 0 156 24" aria-hidden="true" {...props}>
-      <path
-        className="fill-sky-500"
-        d="M10 1L2 4V10.5C2 16.5 5.5 21 10 22.5C14.5 21 18 16.5 18 10.5V4L10 1Z"
-      />
-      <path
-        d="M11 5L7.5 11.5H10.5L8 18.5L15 10.5H11.5L14 5H11Z"
-        fill="white"
-        fillOpacity="0.9"
-      />
+      
       <text
         x="24"
         y="17"
